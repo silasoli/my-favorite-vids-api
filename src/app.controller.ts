f9a36d91284e7e3a3c1e1db99613b645ed172b56
silasoliv39 <silasoliv39@gmail.com>
2023-08-30 20:52:46 -0300

    fix: try redirect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Redirect } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Redirect } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Docs')
 @Controller('docs')
 export class AppController {
   @Get()
-  @Redirect('/docs')
-  redirectToDocs() {}
+  @HttpCode(301) // Use 301 for permanent redirect, or 302 for temporary
+  @Header('Location', '/docs')
+  redirectToDocs() { }
 }
