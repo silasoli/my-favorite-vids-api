ba350c735514b7432bd4199de7ba3c82217c6faa
silasoliv39 <silasoliv39@gmail.com>
2023-11-15 21:34:26 -0300

    fix: remove default img of return

--- src/users/services/user.service.ts
+++ src/users/services/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { User, UserDocument } from '../schemas/user.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
@@ -96,7 +100,8 @@ export class UserService {
 
   public async getAllProfilePictures(): Promise<string[]> {
     const diretorioFotos = join(process.cwd(), 'uploads/profile-picture/');
-    return fs.promises.readdir(diretorioFotos);
+    const items = await fs.promises.readdir(diretorioFotos);
+    return items.filter((item) => item !== 'default-image.png');
   }
 
   public async getProfilePictureByName(name: string): Promise<string> {
